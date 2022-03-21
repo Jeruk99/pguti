ea86aad161e2edb763221c970049955a4138d239
Jeruk99 <vlad_ladzd@mail.ru>
2022-03-21 19:30:53 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+    const double PI = 3.14;
+    double x, y;
+    cout << "Введите x" << endl;
+    cin >> x;
+    y = (sqrt(atan(1 + pow(sin(x), 2)) + log2(1 + exp(-x))))/ (log(pow(sin(x), 2) + PI));
+    cout << "y(x)" << y << endl;
+    system("pause");
+    return 0;
+
+}
+
